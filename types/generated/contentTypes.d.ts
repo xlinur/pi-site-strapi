d53f3d96a7bdbfdb42429a56b45ff2b96c71e92e
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-09-27 21:05:31 +0300

    adds SectionWithFeatures into case page

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -813,6 +813,7 @@ export interface ApiCaseCase extends Schema.CollectionType {
     SectionCompleteTask: Attribute.Component<'sections.section-complete-task'>;
     SectionRecruitmentSummary: Attribute.Component<'sections.section-recruitment-summary'>;
     SectionTalentMatch: Attribute.Component<'sections.section-talent-match'>;
+    SectionWithFeatures: Attribute.Component<'sections.relocation-help-hero'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
