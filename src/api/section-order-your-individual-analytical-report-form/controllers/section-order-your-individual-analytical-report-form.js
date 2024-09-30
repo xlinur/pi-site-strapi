97f7754511e3dd40ac04416a2a628760cf373c1e
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-09-30 19:57:04 +0300

    Revert "adds Order your ind report types"
    This reverts commit f819503643821b2fb2a9ece85921d85d386a7731.

--- src/api/section-order-your-individual-analytical-report-form/controllers/section-order-your-individual-analytical-report-form.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * section-order-your-individual-analytical-report-form controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::section-order-your-individual-analytical-report-form.section-order-your-individual-analytical-report-form');
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1503,49 +1503,6 @@ export interface ApiSectionAboutPersonalinvestSectionAboutPersonalinvest
   };
 }
 
-export interface ApiSectionOrderYourIndividualAnalyticalReportFormSectionOrderYourIndividualAnalyticalReportForm
-  extends Schema.SingleType {
-  collectionName: 'section_order_your_individual_analytical_report_forms';
-  info: {
-    singularName: 'section-order-your-individual-analytical-report-form';
-    pluralName: 'section-order-your-individual-analytical-report-forms';
-    displayName: '[Section] Order your individual analytical report form';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    title: Attribute.String;
-    subTitle: Attribute.String;
-    info: Attribute.Text;
-    contactsTitle: Attribute.String;
-    inputName: Attribute.Component<'molecules.form-input'>;
-    inputCompany: Attribute.Component<'molecules.form-input'>;
-    selectServices: Attribute.Component<'molecules.form-select'>;
-    inputContact: Attribute.Component<'molecules.form-input'>;
-    textareaComment: Attribute.Component<'molecules.form-input'>;
-    legalCheckboxes: Attribute.Component<'molecules.form-checkbox', true>;
-    purposeOfResearch: Attribute.Component<'molecules.form-checkbox', true>;
-    otherPurpose: Attribute.Component<'molecules.form-checkbox'>;
-    submitBtn: Attribute.String;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::section-order-your-individual-analytical-report-form.section-order-your-individual-analytical-report-form',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::section-order-your-individual-analytical-report-form.section-order-your-individual-analytical-report-form',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface ApiSectionPaymentTermsSectionPaymentTerms
   extends Schema.SingleType {
   collectionName: 'section_payment_termss';
@@ -1869,7 +1826,6 @@ declare module '@strapi/types' {
       'api::page-relocation-help.page-relocation-help': ApiPageRelocationHelpPageRelocationHelp;
       'api::page-split-recruitment.page-split-recruitment': ApiPageSplitRecruitmentPageSplitRecruitment;
       'api::section-about-personalinvest.section-about-personalinvest': ApiSectionAboutPersonalinvestSectionAboutPersonalinvest;
-      'api::section-order-your-individual-analytical-report-form.section-order-your-individual-analytical-report-form': ApiSectionOrderYourIndividualAnalyticalReportFormSectionOrderYourIndividualAnalyticalReportForm;
       'api::section-payment-terms.section-payment-terms': ApiSectionPaymentTermsSectionPaymentTerms;
       'api::section-start-conversation-form.section-start-conversation-form': ApiSectionStartConversationFormSectionStartConversationForm;
       'api::section-success-stories.section-success-stories': ApiSectionSuccessStoriesSectionSuccessStories;
