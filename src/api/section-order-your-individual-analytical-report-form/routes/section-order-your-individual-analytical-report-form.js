f819503643821b2fb2a9ece85921d85d386a7731
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-09-30 19:44:35 +0300

    adds Order your ind report types

---
+++ src/api/section-order-your-individual-analytical-report-form/routes/section-order-your-individual-analytical-report-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-order-your-individual-analytical-report-form router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::section-order-your-individual-analytical-report-form.section-order-your-individual-analytical-report-form');
