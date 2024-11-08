5435139e0b6d2bf0864887f7afbab1ebe77b302e
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-11-08 19:58:21 +0300

    updates config

--- config/server.js
+++ config/server.js
@@ -21,6 +21,6 @@ module.exports = ({ env }) => {
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
     },
-    url: env('PUBLIC_URL', 'http://localhost:8089')
+    // url: env('PUBLIC_URL', 'http://localhost:8089')
   }
 };
