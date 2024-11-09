0d1a7271169567d53c1e057ee22a8c2704662784
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-11-10 00:47:21 +0300

    updates config/middleware allow any origin(for a while)

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,8 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      // TODO: change to more secure origin
+      headers: '*',
+      origin: ['*']
+    }
+  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
